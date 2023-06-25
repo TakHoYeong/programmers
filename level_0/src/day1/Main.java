b61f32ad4e98ad1cfb017c839882869d9f98deee
TakHoYeong <xkrghdud123@naver.com>
2023-06-25 12:27:15 +0900

    first commit

---
+++ level_0/src/day1/Main.java
@@ -0,0 +1,14 @@
+package day1;/* 문자열 출력하기 */
+
+// 입력받은 문자열을 출력한다.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        System.out.println(a);
+    }
+}
---
+++ level_0/src/day1/Main2.java
@@ -0,0 +1,17 @@
+/* a와 b 출력하기 */
+
+// 입력받은 정수 a와b를 형식에 맞게 출력
+package day1;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a =" + a);
+        System.out.println("b =" + b);
+    }
+}
