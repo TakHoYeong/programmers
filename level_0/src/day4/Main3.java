87102a742a3c5f8fee37fa84d30922c6eba71e1f
TakHoYeong <xkrghdud123@naver.com>
2023-12-29 15:38:54 +0900

    조건 문자열

--- level_0/src/day4/Main3.java
+++ level_0/src/day4/Main3.java
@@ -1,2 +1,18 @@
-package day4;public class Main3 {
+package day4;
+
+/* 홀짝에 따라 다른 값 반환하기 */
+public class Main3 {
+    public int solution(int n){
+        int answer = 0;
+        if(n%2==1){
+            for(int i=n;  i>0; i-=2){
+                answer += i;
+            }
+        } else {
+            for(int i=n; i>0; i-=2){
+                answer += i*i;
+            }
+        }
+        return answer;
+    }
 }
