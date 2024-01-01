3a12a6df2846aa698d4d447a930b7a21084fb0d8
TakHoYeong <xkrghdud123@naver.com>
2024-01-01 19:49:41 +0900

    주사위 게임2

--- level_0/src/day5/Main2.java
+++ level_0/src/day5/Main2.java
@@ -1,2 +1,13 @@
-package day5;public class Main2 {
+package day5;
+
+/* 등차수열의 특정한 항만 더하기 */
+public class Main2 {
+    public int solution(int a, int d, boolean[] included) {
+        int answer =0;
+        for (int i =0; i < included.length; i++){
+            if (included[i-1] == true)
+                answer += (a + (i-1)*d);
+        }
+        return answer;
+    }
 }
--- level_0/src/day5/Main3.java
+++ level_0/src/day5/Main3.java
@@ -1,2 +1,16 @@
-package day5;public class Main3 {
+package day5;
+
+/* 주사위 게임 2 */
+public class Main3 {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        if (a==b || a==c || b==c){
+            answer = (int) ((a + b + c)*(Math.pow(a, 2)+Math.pow(b, 2)+Math.pow(c,2)));
+        } else if (a==b && b==c) {
+            answer = (int) ((a + b + c)*(Math.pow(a, 2)+Math.pow(b, 2)+Math.pow(c,2))*(Math.pow(a, 3)+Math.pow(b, 3)+Math.pow(c,3)));
+        }
+        else answer = a + b + c;
+
+        return answer;
+    }
 }
