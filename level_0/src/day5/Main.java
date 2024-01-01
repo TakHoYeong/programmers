7a02acb3dd2754d3030ac7cd2eccedebb64325cb
TakHoYeong <xkrghdud123@naver.com>
2024-01-01 17:26:40 +0900

    코드 처리하기

---
+++ level_0/src/day5/Main.java
@@ -0,0 +1,18 @@
+package day5;
+
+/* 코드 처리하기 */
+public class Main {
+    public String solution(String code) {
+        String answer = "";
+        int mode = 0;
+        for (int i = 0; i < code.length(); i++) {
+            if (code.charAt(i) == '1') {
+                mode = 1 - mode;
+            }
+            else if(i % 2 == mode) {
+                answer += code.charAt(i);
+            }
+        }
+        return "".equals(answer) ? "EMPTY": answer;
+    }
+}
