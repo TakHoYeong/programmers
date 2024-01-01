175b8fec37e1a764655d8164d7b533ae44cb2d5a
TakHoYeong <xkrghdud123@naver.com>
2024-01-01 20:29:33 +0900

    이어 붙인 수

--- level_0/src/day5/Main5.java
+++ level_0/src/day5/Main5.java
@@ -1,2 +1,19 @@
-package day5;public class Main5 {
+package day5;
+
+/* 이어 붙인 수 */
+public class Main5 {
+    public int solution(int[] num_list) {
+        String odd = "";
+        String even = "";
+        for (int i=0; i< num_list.length; i++){
+            if (num_list[i]%2==1){
+                odd += num_list[i];
+            }
+            else {
+                even += num_list[i];
+            }
+        }
+        int answer = Integer.valueOf(odd) + Integer.valueOf(even);
+        return answer;
+    }
 }
