a279694d81d52cf52870f32ae7bed1fbbbc5a09f
TakHoYeong <xkrghdud123@naver.com>
2024-01-01 20:19:26 +0900

    원소들의 곱과 합

--- level_0/src/day5/Main4.java
+++ level_0/src/day5/Main4.java
@@ -1,2 +1,20 @@
-package day5;public class Main4 {
+package day5;
+
+/* 원소들의 곱과 합 */
+public class Main4 {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int multiply = 1;
+        int square = 0;
+        int sum = 0;
+        for(int i=0; i < num_list.length; i++){
+            multiply *= num_list[i];
+            sum += num_list[i];
+        }
+        square += Math.pow(sum, 2);
+        if(multiply < square)
+            answer = 1;
+
+        return answer;
+    }
 }
