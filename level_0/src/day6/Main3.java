ab96f92af0d35fb6d03c8a75b45833ce26da8567
TakHoYeong <xkrghdud123@naver.com>
2024-01-02 15:50:24 +0900

    수 조작하기 2

--- level_0/src/day6/Main3.java
+++ level_0/src/day6/Main3.java
@@ -1,2 +1,19 @@
-package day6;public class Main3 {
+package day6;
+
+/* 수 조작하기 2 */
+public class Main3 {
+    public String solution(int[] numLog) {
+        String answer = "";
+        for(int i=1; i<= numLog.length-1; i++) {
+            if (numLog[i]-numLog[i-1] == 1)
+                answer += "w";
+            else if (numLog[i]-numLog[i-1] == -1)
+                answer += "s";
+            else if (numLog[i]-numLog[i-1] == 10)
+                answer += "d";
+            else if (numLog[i]-numLog[i-1] == -10)
+                answer += "a";
+        }
+        return answer;
+    }
 }
