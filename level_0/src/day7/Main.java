a69ddd6ed7846b68bad435deba19b0bab7867788
TakHoYeong <xkrghdud123@naver.com>
2024-01-03 16:59:28 +0900

    수열과 구간 쿼리 4

---
+++ level_0/src/day7/Main.java
@@ -0,0 +1,14 @@
+package day7;
+
+/* 수열과 구간 쿼리 4 */
+public class Main {
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int i=0; i< queries.length; i++){
+            for (int j= queries[i][0]; j<=queries[i][1]; j++){
+                if (j% queries[i][2] == 0)
+                    arr[j]++;
+            }
+        }
+        return arr;
+    }
+}
