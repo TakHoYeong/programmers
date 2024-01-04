39828f5ac544a0abe1a8d7263d0c94ab71bc0451
TakHoYeong <xkrghdud123@naver.com>
2024-01-04 12:40:27 +0900

    9로 나눈 나머지

--- level_0/src/day8/Main4.java
+++ level_0/src/day8/Main4.java
@@ -1,2 +1,14 @@
-package day8;public class Main4 {
+package day8;
+
+/* 9로 나눈 나머지 */
+public class Main4 {
+    public int solution(String number) {
+        String[] num_list = number.split("");
+        int answer = 0;
+        for (int i=0; i<num_list.length; i++){
+            answer += Integer.parseInt(num_list[i]);
+        }
+        answer %= 9;
+        return answer;
+    }
 }
