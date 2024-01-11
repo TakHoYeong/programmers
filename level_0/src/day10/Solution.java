66172deaaece17823bd620d004f250e235c32768
TakHoYeong <xkrghdud123@naver.com>
2024-01-11 13:29:20 +0900

    문자열 뒤의 n글자

---
+++ level_0/src/day10/Solution.java
@@ -0,0 +1,9 @@
+package day10;
+
+/* 문자열 뒤의 n글자 */
+public class Solution {
+    public String solution(String my_string, int n) {
+        String answer = my_string.substring(my_string.length()-n, my_string.length());
+        return answer;
+    }
+}
